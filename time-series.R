2f5d9aee8e170379073dc140a2ce9b0162466c3c
David Snyder <davidsnyder@loki.local>
2016-11-05 22:32:15 +0900

    add mk64 times

--- time-series.R
+++ time-series.R
@@ -1,12 +1,22 @@
+#!/usr/bin/env Rscript
+
+args = commandArgs(trailingOnly=TRUE)
+
+# input output
+if (length(args) < 2) {
+  stop("Provide input output", call.=FALSE)
+}
+
 library(tidyverse)
 library(lubridate)
+options(digits.secs=3)
 
-pdf('graph.pdf')
+pdf(args[2])
 
-loz <- read.table("loz_times.tsv",header = TRUE)
-floz <- loz %>% select(format_time,date,player) %>% mutate(fdate = mdy(date)) #objectify date
-floz$format_time <- as.POSIXct(floz$format_time, format = "%H:%M:%S") #must convert to date, even though we only use hms
-graph <- ggplot(data = floz) + geom_point(mapping = aes(x = fdate, y = format_time, color = player))
+table <- read.table(args[1], sep = "\t", header = TRUE)
+ftable <- table %>% select(format_time,date,player) %>% mutate(fdate = mdy(date)) #objectify date
+ftable$format_time <- as.POSIXct(ftable$format_time, format = "%H:%M:%OS") #must convert to date, even though we only use hms
+graph <- ggplot(data = ftable) + geom_point(mapping = aes(x = fdate, y = format_time, color = player))
 
 print(graph)
 
