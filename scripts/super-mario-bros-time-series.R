fa80c4f5a6e3180282d5dab33bbfec3f33ea2b7b
David Snyder <davidsnyder@loki.local>
2017-01-21 00:24:06 +0900

    Add SMB script,data as well as legends R code

--- scripts/super-mario-bros-time-series.R
+++ scripts/super-mario-bros-time-series.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 
-graph_title = "Super Metroid Any% Records"
+graph_title = "Super Mario Bros. Any% Records"
 args = commandArgs(trailingOnly=TRUE)
 
 # input output
@@ -13,15 +13,15 @@ library(tidyverse)
 library(lubridate)
 options(digits.secs=3)
 
-pdf(args[2],10)
+pdf(args[2],width=12)
 
 table <- read.table(args[1], sep = "\t", header = TRUE)
-ftable <- table %>% select(format_time,date,player) %>% mutate(fdate = mdy(date)) #objectify date
+ftable <- table %>% select(format_time,date,player,feature) %>% mutate(fdate = mdy(date)) #objectify date
 ftable$format_time <- as.POSIXct(ftable$format_time, format = "%H:%M:%OS") #must convert to datetime, even though we only use hms
 graph <- ggplot(data = ftable) +
       	 geom_line(mapping = aes(x = fdate, y = format_time)) +
 	 geom_point(mapping = aes(x = fdate, y = format_time,shape=player),size=3) +
-	 scale_shape_manual(values=c(16,2,15,17,10)) +
+	 scale_shape_manual(values=c(16,10,15,17,7,2)) +
 	 labs(shape="") +
 	 ggtitle(graph_title) +
 	 theme(plot.title = element_text(hjust = 0.5)) +
@@ -34,10 +34,10 @@ graph <- ggplot(data = ftable) +
 	 theme(axis.title.x=element_text(margin=margin(15,0,0,0))) + #increase margin for x-axis label
 	 theme(plot.margin=unit(c(1,1,1,1),"cm")) + #increase margin size around whole graph
 #	 geom_text_repel(data=subset(ftable, player=="Zoast"), aes(fdate,format_time,label=player), size=3) + #label all points matching player
-#	 geom_text_repel(data=subset(ftable, date=="9/2/2016"), aes(fdate,format_time,label=player),size=4,nudge_y=100,point.padding=unit(0.25,'lines'),box.padding=unit(2,'lines')) + #label all points matching date
+	 geom_text_repel(data=subset(ftable, feature!=""), aes(fdate,format_time,label=feature),size=4,point.padding=unit(0.75,'lines'),box.padding=unit(2,'lines')) + #label all points with "feature" column
 #	 geom_vline(xintercept=as.numeric(mdy("10/19/2014")), linetype="dashed") + #how to make a dashed vline
-#	 geom_hline(yintercept=as.numeric(as.POSIXct("2017-01-14 00:44:29")),linetype="dashed") + #how to make a dashed hline
-	 scale_y_datetime(date_labels = "%Mm") 
+#	 geom_hline(yintercept=as.numeric(as.POSIXct("2017-01-20 00:4:29")),linetype="dashed") + #how to make a dashed hline
+	 scale_y_datetime(date_labels = "%Mm%Ss") 
 #	 scale_y_datetime(date_labels = "%M\'%S\"") 
 
 print(graph)
