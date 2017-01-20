48f4c1698f4c988f917a63adf2084a44920140cd
David Snyder <davidsnyder@loki.local>
2017-01-21 00:49:17 +0900

    Add SMB TAS time

--- scripts/super-mario-bros-time-series.R
+++ scripts/super-mario-bros-time-series.R
@@ -15,13 +15,16 @@ options(digits.secs=3)
 
 pdf(args[2],width=12)
 
+tas <- data.frame(format_time=as.POSIXct("0:04:54.03",format="%H:%M:%OS"),fdate=mdy("01/06/2011"),player="HappyLee",feature="TAS")
+
 table <- read.table(args[1], sep = "\t", header = TRUE)
 ftable <- table %>% select(format_time,date,player,feature) %>% mutate(fdate = mdy(date)) #objectify date
 ftable$format_time <- as.POSIXct(ftable$format_time, format = "%H:%M:%OS") #must convert to datetime, even though we only use hms
 graph <- ggplot(data = ftable) +
       	 geom_line(mapping = aes(x = fdate, y = format_time)) +
 	 geom_point(mapping = aes(x = fdate, y = format_time,shape=player),size=3) +
-	 scale_shape_manual(values=c(16,10,15,17,7,2)) +
+	 geom_point(data=tas,aes(x=fdate,y=format_time,shape=player),size=3) +
+	 scale_shape_manual(values=c(16,10,15,17,7,2,0)) +
 	 labs(shape="") +
 	 ggtitle(graph_title) +
 	 theme(plot.title = element_text(hjust = 0.5)) +
@@ -34,6 +37,7 @@ graph <- ggplot(data = ftable) +
 	 theme(axis.title.x=element_text(margin=margin(15,0,0,0))) + #increase margin for x-axis label
 	 theme(plot.margin=unit(c(1,1,1,1),"cm")) + #increase margin size around whole graph
 #	 geom_text_repel(data=subset(ftable, player=="Zoast"), aes(fdate,format_time,label=player), size=3) + #label all points matching player
+	 geom_text_repel(data=tas, aes(fdate,format_time,label=feature), size=4,box.padding=unit(2,'lines'),point.padding=unit(0.75,'lines'),nudge_x=-10) +
 	 geom_text_repel(data=subset(ftable, feature!=""), aes(fdate,format_time,label=feature),size=4,point.padding=unit(0.75,'lines'),box.padding=unit(2,'lines')) + #label all points with "feature" column
 #	 geom_vline(xintercept=as.numeric(mdy("10/19/2014")), linetype="dashed") + #how to make a dashed vline
 #	 geom_hline(yintercept=as.numeric(as.POSIXct("2017-01-20 00:4:29")),linetype="dashed") + #how to make a dashed hline
