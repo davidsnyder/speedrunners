7790da625c33f80fda985ccf41869d3710e00bf1
David Snyder <davidsnyder@loki.local>
2017-02-24 22:53:38 +0900

    Add OOT script

--- scripts/oot-time-series.R
+++ scripts/oot-time-series.R
@@ -2,7 +2,7 @@
 
 #Use data file "loz-top3.tsv" with this R script
 
-graph_title = "The Legend of Zelda Any% World Record Competition"
+graph_title = "The LoZ: Ocarina of Time 100% Top 3 Competition"
 args = commandArgs(trailingOnly=TRUE)
 
 # input output
@@ -18,30 +18,30 @@ options(digits.secs=3)
 pdf(args[2],width=8)
 
 table <- read.table(args[1], sep = "\t", header = TRUE)
-ftable <- table %>% select(format_time,date,player) %>% mutate(fdate = mdy(date)) #objectify date
+ftable <- table %>% select(player,format_time,date,console) %>% mutate(fdate = mdy(date)) #objectify date
 ftable$format_time <- as.POSIXct(ftable$format_time, format = "%H:%M:%OS") #must convert to datetime, even though we only use hms
 
 #top 3
-lack <- subset(ftable, player=="LackAttack24")
-darkwing <- subset(ftable, player=="Darkwing_Duck_sda")
-cantaloupe <- subset(ftable, player=="cantaloupeme")
+clint <- subset(ftable, player=="ClintStevens")
+zfg <- subset(ftable, player=="ZFG")
+sva <- subset(ftable, player=="sva")
 
-tas <- data.frame(format_time=as.POSIXct("0:22:17.53",format="%H:%M:%OS"),fdate=mdy("09/29/2016"),player="Lord Tom (TAS)",feature="TAS")
+tas <- data.frame(format_time=as.POSIXct("3:47:57.17",format="%H:%M:%OS"),fdate=mdy("07/06/2015"),player="MoronicTAS & GCTK24 (TAS)",feature="TAS")
 
 graph <- ggplot() +
-      	 #lack
-      	 geom_line(data=lack, mapping = aes(x = fdate, y = format_time),linetype=1) +
-	 geom_point(data=lack, mapping = aes(x = fdate, y = format_time,shape=player),size=1) +
-	 #darkwing
-      	 geom_line(data=darkwing, mapping = aes(x = fdate, y = format_time),linetype=2) +
-	 geom_point(data=darkwing, mapping = aes(x = fdate, y = format_time,shape=player),size=1) +
-	 #cantaloupe
-      	 geom_line(data=cantaloupe, mapping = aes(x = fdate, y = format_time),linetype=3) +
-	 geom_point(data=cantaloupe, mapping = aes(x = fdate, y = format_time,shape=player),size=1) +
+      	 #clint
+      	 geom_line(data=clint, mapping = aes(x = fdate, y = format_time),linetype=1) +
+	 geom_point(data=clint, mapping = aes(x = fdate, y = format_time,shape=player),size=1) +
+	 #zfg
+      	 geom_line(data=zfg, mapping = aes(x = fdate, y = format_time),linetype=2) +
+	 geom_point(data=zfg, mapping = aes(x = fdate, y = format_time,shape=player),size=1) +
+	 #sva
+      	 geom_line(data=sva, mapping = aes(x = fdate, y = format_time),linetype=3) +
+	 geom_point(data=sva, mapping = aes(x = fdate, y = format_time,shape=player),size=1) +
 	 #tas
-#	 geom_point(data=tas,aes(x=fdate,y=format_time,shape=player),size=3) +
+	 geom_point(data=tas,aes(x=fdate,y=format_time,shape=player),size=3) +
 
-	 scale_shape_manual(values=c(16,17,15,10)) +
+	 scale_shape_manual(values=c(16,17,15,1)) +
 	 labs(shape="") +
 	 ggtitle(graph_title) +
 	 theme(plot.title = element_text(hjust = 0.5)) +
@@ -57,8 +57,8 @@ graph <- ggplot() +
 #	 geom_text_repel(data=subset(ftable, date=="9/2/2016"), aes(fdate,format_time,label=player),size=4,nudge_y=100,point.padding=unit(0.25,'lines'),box.padding=unit(2,'lines')) + #label all points matching date
 #	 geom_vline(xintercept=as.numeric(mdy("10/19/2014")), linetype="dashed") + #how to make a dashed vline
 #	 geom_hline(yintercept=as.numeric(as.POSIXct("2017-01-14 00:44:29")),linetype="dashed") + #how to make a dashed hline
-	 scale_y_datetime(date_labels = "%Mm") #y axis labels for runs < 1hr
-#	 scale_y_datetime(date_labels = "%Hh%Mm") #y axis labels for runs with hours
+#	 scale_y_datetime(date_labels = "%Mm") #y axis labels for runs < 1hr
+	 scale_y_datetime(date_labels = "%Hh%Mm") #y axis labels for runs with hours
 #	 scale_y_datetime(date_labels = "%M\'%S\"") 
 
 print(graph)
