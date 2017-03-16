0781fc5893b84c51f4fbac573de7ba348b038338
David Snyder <davidsnyder@loki.local>
2017-03-17 07:23:12 +0900

    First attempt at mk64 graphs

--- scripts/mk64-all-cups-skips.R
+++ scripts/mk64-all-cups-skips.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 
-graph_title = "Mario Kart 64 Kalimari Desert World Record Progression"
-graph_subtitle = "Time Trials 3lap (PAL Times)"
+graph_title = "Mario Kart 64 All Cups World Record Progression"
+graph_subtitle = "NTSC 150cc Skips"
 args = commandArgs(trailingOnly=TRUE)
 
 # input output
@@ -17,21 +17,13 @@ options(digits.secs=3)
 pdf(args[2])
 
 table <- read.table(args[1], sep = "\t", header = TRUE)
-ftable <- table %>% select(date,format_time,player,system,shortcut) %>% mutate(fdate = mdy(date)) #objectify date
+ftable <- table %>% select(format_time,player,date,notes) %>% mutate(fdate = mdy(date)) #objectify date
 ftable$format_time <- as.POSIXct(ftable$format_time, format = "%H:%M:%OS") #must convert to datetime, even though we only use hms
 
-shortcut <- subset(ftable, shortcut=="YES")
-non_shortcut <- subset(ftable, shortcut!="YES")
-
-recent_points <- subset(ftable, (player=="abney317" | player=="MR" | player=="MJ" | player=="VAJ" ))
-
-graph <- ggplot() +
-      	 geom_line(data=shortcut,mapping = aes(x = fdate, y = format_time,linetype=shortcut)) +
-      	 geom_line(data=non_shortcut,mapping = aes(x = fdate, y = format_time,linetype=shortcut)) +
-	 
-	 geom_point(data=recent_points,mapping = aes(x = fdate, y = format_time,shape=player),size=1.75) +
-	 scale_linetype_manual("Category", values=c("solid", "dotted")) +	 
-	 scale_shape_manual("Player",values=c(0,1,15,16)) +
+graph <- ggplot(data=ftable) +
+      	 geom_line(mapping = aes(x = fdate, y = format_time)) +
+	 geom_point(mapping = aes(x = fdate, y = format_time,shape=player),size=2) +
+	 scale_shape_manual(values=c(0,1,15,16)) +
 	 labs(shape="") +
 	 ggtitle(graph_title,graph_subtitle) +
 	 theme(plot.title = element_text(hjust = 0.5)) +
@@ -45,12 +37,12 @@ graph <- ggplot() +
 	 theme(axis.title.x=element_text(margin=margin(15,0,0,0))) + #increase margin for x-axis label
 	 theme(plot.margin=unit(c(1,1,1,1),"cm")) + #increase margin size around whole graph
 #	 geom_text_repel(data=subset(ftable, player=="Zoast"), aes(fdate,format_time,label=player), size=3) + #label all points matching player
-#	 geom_text_repel(data=subset(ftable, date=="8/28/2014"), aes(fdate,format_time,label="PRKD change"),size=4,nudge_x=200,nudge_y=100,point.padding=unit(0.25,'lines'),box.padding=unit(2,'lines')) + #label all points matching date
+	 geom_text_repel(data=ftable, aes(fdate,format_time,label=notes),size=3,point.padding=unit(0.6,'lines'),box.padding=unit(1,'lines')) + #label all points matching date
 #	 geom_text_repel(data=subset(ftable, date=="6/6/2012"), aes(fdate,format_time,label="Tie with Hotarubi"),size=4,nudge_x=200,nudge_y=100,point.padding=unit(0.25,'lines'),box.padding=unit(2,'lines')) + #label all points matching date	 
 #	 geom_vline(xintercept=as.numeric(mdy("10/19/2014")), linetype="dashed") + #how to make a dashed vline
 #	 geom_hline(yintercept=as.numeric(as.POSIXct("2017-01-14 00:44:29")),linetype="dashed") + #how to make a dashed hline
-#	 scale_y_datetime(date_labels = "%Mm%Ss",breaks = scales::pretty_breaks(n = 8)) 
-	 scale_y_datetime(limits=as.POSIXct(strptime(c("02:02.3","02:09.5"), format = "%M:%OS")), date_labels = "%Mm%Ss",breaks = scales::pretty_breaks(n = 8)) 
+	 scale_y_datetime(date_labels = "%Mm",breaks = scales::pretty_breaks(n = 8))
+#	 scale_y_datetime(limits=as.POSIXct(strptime(c("02:04","02:09"), format = "%M:%S")), date_labels = "%Mm%Ss",breaks = scales::pretty_breaks(n = 8)) 
 #	 coord_cartesian(xlim = as.Date(c("2015-10-01", "2016-11-01")),ylim = as.POSIXct(strptime(c("02:04","02:05"), format = "%M:%S")))
 #	 scale_y_datetime(date_labels = "%M\'%S\"") 
 
